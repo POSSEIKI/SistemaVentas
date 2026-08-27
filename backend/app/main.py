8bf6c853a5a5674b4d3e4e370b67d76d22bd22da
Felipe <felipe@ejemplo.com>
2026-08-26 23:03:51 -0500

    fix(cors): allow all origin regex for Vercel domains

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,7 +20,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origins,
+    allow_origin_regex=r".*",
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
